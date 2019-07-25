7a96fba999530a8cc82e8b7f2adac63d6e297580
BeM835 <turejskijleonid004@gmail.com>
2019-07-26 04:01:17 +1000

    Now cant divide by zero

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main()
     char operator = "+";
     int n2;
     int output;
+    int pr_out = 1;
  
     printf("Enter first integer: ");  
     
@@ -19,16 +20,23 @@ int main()
     scanf("%d", &n2);
     
     if(operator == '+'){
-      output = n1 + n2;
+		output = n1 + n2;
     }else if(operator == '-'){
-      output = n1 - n2;
+		output = n1 - n2;
     }else if(operator == '*'){
-      output = n1 * n2;
+		output = n1 * n2;
     }else if(operator == '/'){
-      output = n1 / n2;
+    	if (n2 == 0){
+    		printf("You cant divide by 0!!!11!\n");
+    		pr_out = 0;
+    	}else{
+			output = n1 / n2;
+		}
     }/*else if(operator == '^'){
       output = power(n1, n2);
     }*/
-    printf("%d %c %d = %d", n1, operator, n2, output);
+	if(pr_out == 1){
+    	printf("%d %c %d = %d", n1, operator, n2, output);
+	}
     return 0;
 }
