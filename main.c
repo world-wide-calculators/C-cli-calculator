7061f7e828f4e21dd5ed79bfd45fa366836be7f6
BeM835 <turejskijleonid004@gmail.com>
2019-07-26 04:08:27 +1000

    retry added

--- main.c
+++ main.c
@@ -5,38 +5,43 @@ int main()
     int n1;
     int op;
     char operator = "+";
+    char retry;
     int n2;
     int output;
     int pr_out = 1;
- 
-    printf("Enter first integer: ");  
-    
-    scanf("%d", &n1);  
-    
-    printf("Enter an operator: ");
-    scanf(" %c", &operator);
-    
-    printf("Enter second integer: ");
-    scanf("%d", &n2);
-    
-    if(operator == '+'){
-		output = n1 + n2;
-    }else if(operator == '-'){
-		output = n1 - n2;
-    }else if(operator == '*'){
-		output = n1 * n2;
-    }else if(operator == '/'){
-    	if (n2 == 0){
-    		printf("You cant divide by 0!!!11!\n");
-    		pr_out = 0;
-    	}else{
-			output = n1 / n2;
+ 	
+ 	do{
+	    printf("Enter first integer: ");  
+	    
+	    scanf("%d", &n1);  
+	    
+	    printf("Enter an operator: ");
+	    scanf(" %c", &operator);
+	    
+	    printf("Enter second integer: ");
+	    scanf("%d", &n2);
+	    
+	    if(operator == '+'){
+			output = n1 + n2;
+	    }else if(operator == '-'){
+			output = n1 - n2;
+	    }else if(operator == '*'){
+			output = n1 * n2;
+	    }else if(operator == '/'){
+	    	if (n2 == 0){
+	    		printf("You cant divide by 0!!!11!\n");
+	    		pr_out = 0;
+	    	}else{
+				output = n1 / n2;
+			}
+	    }/*else if(operator == '^'){
+	      output = power(n1, n2);
+	    }*/
+		if(pr_out == 1){
+	    	printf("%d %c %d = %d\n", n1, operator, n2, output);
 		}
-    }/*else if(operator == '^'){
-      output = power(n1, n2);
-    }*/
-	if(pr_out == 1){
-    	printf("%d %c %d = %d", n1, operator, n2, output);
-	}
+		printf("Retry? (y/n)  ");
+	    scanf(" %c", &retry);
+	}while(retry == 'y');
     return 0;
 }
