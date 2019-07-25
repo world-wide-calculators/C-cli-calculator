dfe5c886211fa6ce99511be0cf03a6c220461ec0
BeM835 <turejskijleonid004@gmail.com>
2019-07-26 04:16:34 +1000

    Power added

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int n1;
-    int op;
+    float n1;
     char operator = "+";
     char retry;
-    int n2;
-    int output;
+    float n2;
+    float output;
     int pr_out = 1;
  	
  	do{
 	    printf("Enter first integer: ");  
 	    
-	    scanf("%d", &n1);  
+	    scanf("%f", &n1);  
 	    
 	    printf("Enter an operator: ");
 	    scanf(" %c", &operator);
 	    
 	    printf("Enter second integer: ");
-	    scanf("%d", &n2);
+	    scanf("%f", &n2);
 	    
 	    if(operator == '+'){
 			output = n1 + n2;
@@ -34,11 +34,11 @@ int main()
 	    	}else{
 				output = n1 / n2;
 			}
-	    }/*else if(operator == '^'){
-	      output = power(n1, n2);
-	    }*/
+	    }else if(operator == '^'){
+	      output = powf(n1, n2);
+	    }
 		if(pr_out == 1){
-	    	printf("%d %c %d = %d\n", n1, operator, n2, output);
+	    	printf("%f %c %f = %f\n\n", n1, operator, n2, output);
 		}
 		printf("Retry? (y/n)  ");
 	    scanf(" %c", &retry);
